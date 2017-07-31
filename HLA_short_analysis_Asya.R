d2d256543edc8fc5529194043c5777851f085138
pogorely <m.pogorely@gmail.com>
2017-07-31 12:20:35 +0300

    minor fix

--- HLA_short_analysis_Asya.R
+++ HLA_short_analysis_Asya.R
@@ -49,14 +49,14 @@ File_list_pipeline_amplicones_parallel<-function(filelist,read_length=250,thresh
 File_list_pipeline_amplicones_parallel_win<-function(filelist,read_length=250,threshold=100,cores=4){
   resHLA<-lapply(filelist[,1],"[",1)
   names(resHLA)<-filelist[,1]
-  cl <- makePSOCKcluster(cores)
-  setDefaultCluster(cl)
-  res<-parLapply(cl,1:nrow(filelist), mc.cores =cores, function(i){
-    cat(names(resHLA)[i])
+  #cl <- makePSOCKcluster(cores)
+  #setDefaultCluster(cl)
+  res<-foreach(i=1:nrow(filelist),.inorder = T)%dopar%{
+   # cat(names(resHLA)[i])
     HLA_amplicones_full(filelist[i,2],filelist[i,3],read_length = read_length,threshold=threshold)
-  })
+  }
   names(res)<-filelist[,1]
-  stopCluster(cl)
+  #stopCluster(cl)
   res
 }
 
